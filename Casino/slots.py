802ce0b6c9ab58774d084d1becf1337f741546ec
Wifi <oliwier.zar@wp.pl>
2025-10-08 23:47:33 +0200

    Create slots.py

---
+++ Casino/slots.py
@@ -0,0 +1,11 @@
+def spin_row():
+    
+def print_row():
+
+def get_payout():
+
+def spin_slots(wallet):
+    print("*************************")
+    print("Welcome to Python Slots ")
+    print("Symbols: 🍒 🍉 🍋 🔔 ⭐")
+    print("*************************")
