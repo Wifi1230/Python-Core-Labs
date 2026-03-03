8e8fcc5b97e56688847434a75827a624c6da42ef
Wifi1230 <oliwier.zar@wp.pl>
2026-03-03 10:31:57 +0100

    Slots added to casino

--- Casino/menu.py
+++ Casino/menu.py
@@ -1,5 +1,6 @@
 import diceroll
 import blackjack
+import slots
 
 while True:
     wallet=input("How much do you want to deposit: ")
@@ -12,8 +13,9 @@ while True:
     print("\n=== MENU ===")
     print("1. 🎲 Play Dice")
     print("2. ♠️ Play Black Jack")
-    print("3. 💰 Show Balance")
-    print("4. 🚪 Exit Casino")
+    print("3. 🍒 Play Slots")
+    print("4. 💰 Show Balance")
+    print("5. 🚪 Exit Casino")
 
     choice = input("Choose option: ")
 
@@ -21,9 +23,11 @@ while True:
         wallet=diceroll.dice_roll(wallet)
     elif choice=="2":
         wallet=blackjack.black_jack(wallet)
-    elif choice =="3":
+    elif choice=="3":
+        wallet=slots.slots_game(wallet)
+    elif choice =="4":
         print(f"\nYour balance is {wallet:.2f}$")
-    elif choice == "4":
+    elif choice == "5":
         print(f"Bye bye! Your payout is {wallet:.2f}$")
         break
     else:
