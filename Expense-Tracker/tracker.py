b32363410d13070ba4e2e8cff93315b54f573fae
Wifi1230 <oliwier.zar@wp.pl>
2026-03-07 20:36:24 +0100

    feat: add CSV data persistence and timestamping for expenses

--- Expense-Tracker/tracker.py
+++ Expense-Tracker/tracker.py
@@ -1,25 +1,46 @@
+import csv
 from collections import defaultdict
+from datetime import datetime
+
 class Expense:
-    def __init__(self, item, price, category):
+    def __init__(self, item, price, category,date):
         self.item = item
         self.price = price
         self.category = category
+        self.date=date
 
 class BankAccount:
-    def __init__(self):
-        self._expenses= defaultdict(list) 
-    
+    def __init__(self, filename="expenses.csv"):
+        self.filename = filename
+        self._expenses= defaultdict(list)
+        self.setup_csv()
+
+    def setup_csv(self):
+        try:
+            with open(self.filename, 'x', newline='', encoding='utf-8') as f:
+                writer = csv.writer(f)
+                writer.writerow(["Date", "Item", "Price", "Category"])
+        except FileExistsError:
+            pass
+
     def add_expense (self,item,price,category):
-        new_expense=Expense(item,price,category)
+        date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        new_expense=Expense(item,price,category,date)
         self._expenses[category].append(new_expense)
         print(f"Added: {item} to {category}")
+
+        with open(self.filename, 'a', newline='', encoding='utf-8') as f:
+            writer = csv.writer(f)
+            writer.writerow([date,item,price,category])
+            
+        print(f"\n[v] Saved to CSV: {item} (${price:.2f})")
 
     def __call__(self):
         print("\n--- EXPENSE REPORT ---")
         total = 0
         for category, list_of_expenses in self._expenses.items():
             for expense in list_of_expenses:
-                print(f"[{category}] {expense.item}: ${expense.price:.2f}")
+                print(f"[{expense.date}] [{category}] {expense.item}: ${expense.price:.2f}")
                 total += expense.price
         print(f"----------------------\nTOTAL: ${total:.2f}")
 
